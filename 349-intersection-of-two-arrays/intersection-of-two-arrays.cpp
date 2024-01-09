8661ec59c470f98f1b868714c779e77d6e1f01bf
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-01-09 23:48:02 +0530

    Time: 7 ms (45.10%) | Memory: 10.3 MB (89.63%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v(0);
-        map<int, int> ump;
-        int n = nums1.size();
-        int m = nums2.size();
-        for(int i=0;i<n;i++){
-            ump[nums1[i]]++;
-        }
+        // Brute Force Approach -> using extra space.
+        // vector<int> v(0);
+        // map<int, int> ump;
+        // int n = nums1.size();
+        // int m = nums2.size();
+        // for(int i=0;i<n;i++){
+        //     ump[nums1[i]]++;
+        // }
 
         
     
-        for(int i=0;i<m;i++){
-            if(ump.find(nums2[i]) != ump.end()){
-                v.push_back(nums2[i]);
-                ump.erase(nums2[i]);
+        // for(int i=0;i<m;i++){
+        //     if(ump.find(nums2[i]) != ump.end()){
+        //         v.push_back(nums2[i]);
+        //         ump.erase(nums2[i]);
+        //     }
+        // }
+        // for(auto i : ump){
+        //     cout<<i.first<<" --> "<<i.second<<endl;
+        // }
+        // sort(v.begin(), v.end());
+
+        // return v;
+
+        vector<int> v;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int n = nums1.size();
+        int m = nums2.size();
+
+        int one = 0, two = 0;
+        while(one < n and two < m){
+            if(nums1[one] < nums2[two]){
+                one++;
+            }
+            else if(nums1[one] > nums2[two]){
+                two++;
+            }
+            else{
+                if(v.size() == 00 or v.back() != nums1[one]){
+                    v.push_back(nums1[one]);
+                }
+                one++;
+                two++;
             }
         }
-        for(auto i : ump){
-            cout<<i.first<<" --> "<<i.second<<endl;
-        }
-        sort(v.begin(), v.end());
-
         return v;
     }
 };
