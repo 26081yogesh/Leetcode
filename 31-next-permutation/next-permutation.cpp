73986eb12937bd2c05d038a32d9037a9ed5e4ab8
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-01-18 00:40:16 +0530

    Time: 4 ms (56.06%) | Memory: 12.4 MB (56.14%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        // Brute force approach
+        // next_permutation(nums.begin(), nums.end());
+
+
+
+        // Optimal Approach
+        int index = -1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                index = i+1;
+                break;
+            }
+        }
+
+        int toswap = index-1;
+        if(index == -1){
+            reverse(nums.begin(), nums.end());
+        }else{
+            for(int i=nums.size()-1;i>=0;i--){
+                if(nums[i] > nums[toswap]){
+                    swap(nums[i], nums[toswap]);
+                    break;
+                }
+            }
+            int start = index;
+            int end = nums.size()-1;
+            while(start < end){
+                swap(nums[start++], nums[end--]);
+            }
+        }
+
     }
 };
