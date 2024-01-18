6208f2a8c3537d3c3bd5c06a7efb6e93fa93add3
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-01-18 11:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (83.13%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,7 +11,45 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> v;
+        // vector<int> v;
+        // if(list1 == NULL and list2 == NULL){
+        //     return NULL;
+        // }
+        // if(list1 == NULL and list2 != NULL){
+        //     return list2;
+        // }
+        // if(list1 != NULL and list2 == NULL){
+        //     return list1;
+        // }
+        // ListNode *temp = list1;
+        // while(temp != NULL){
+        //     v.push_back(temp -> val);
+        //     temp = temp -> next;
+        // }
+        // temp = list2;
+        // while(temp != NULL){
+        //     v.push_back(temp -> val);
+        //     temp = temp -> next;
+        // }
+
+        // sort(v.begin(), v.end());
+        // ListNode *head = NULL;
+        // for(auto i:v){
+        //     ListNode *newNode = new ListNode(i);
+        //     if(head == NULL){
+        //         head = newNode;
+        //         temp = head;
+        //     }
+        //     else{
+        //         temp -> next = newNode;
+        //         temp = newNode;
+        //     }
+        // }
+        // return head;
+
+
+
+
         if(list1 == NULL and list2 == NULL){
             return NULL;
         }
@@ -21,30 +59,36 @@ public:
         if(list1 != NULL and list2 == NULL){
             return list1;
         }
-        ListNode *temp = list1;
-        while(temp != NULL){
-            v.push_back(temp -> val);
-            temp = temp -> next;
+
+
+        ListNode *ptr = list1;
+        if(list1 -> val > list2 -> val){
+            ptr = list2;
+            list2 = list2 -> next;
         }
-        temp = list2;
-        while(temp != NULL){
-            v.push_back(temp -> val);
+        else{
+            list1 = list1 -> next;
+        }
+
+        ListNode *temp = ptr;
+        while(list1 != NULL and list2 != NULL){
+            if(list1 -> val < list2 -> val){
+                temp -> next = list1;
+                list1 = list1 -> next;
+            }
+            else{
+                temp -> next = list2;
+                list2 = list2 -> next;
+            }
             temp = temp -> next;
         }
 
-        sort(v.begin(), v.end());
-        ListNode *head = NULL;
-        for(auto i:v){
-            ListNode *newNode = new ListNode(i);
-            if(head == NULL){
-                head = newNode;
-                temp = head;
-            }
-            else{
-                temp -> next = newNode;
-                temp = newNode;
-            }
+        if(!list1){
+            temp -> next = list2;
         }
-        return head;
+        else{
+            temp -> next = list1;
+        }
+        return ptr;
     }
 };
