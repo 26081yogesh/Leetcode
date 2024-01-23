3e80e593f14029eb2b5c123d95d055e94c32b5e4
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-01-23 23:14:33 +0530

    Time: 26 ms (42.28%) | Memory: 76.2 MB (6.64%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseLL(ListNode *head){
+        ListNode *current = head;
+        ListNode *previous = NULL;
+        ListNode *forward = NULL;
+        while(current != NULL){
+            forward = current -> next;
+            current -> next = previous;
+            previous = current;
+            current = forward;
+        }
+        return previous;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // ListNode *L1 = reverseLL(l1);
+        // ListNode *L2 = reverseLL(l2);
+
+        ListNode *temp1 = l1;
+        ListNode *temp2 = l2;
+        ListNode *newNode = new ListNode(-100);
+        ListNode *head = newNode;
+        ListNode *temp = head;
+
+        int carry = 0;
+        while(temp1 != NULL and temp2 != NULL){
+            int sum = temp1 -> val + temp2 -> val;
+            sum += carry;
+            ListNode *newNode = new ListNode(sum % 10);
+            temp -> next = newNode;
+            carry = sum / 10;
+            temp = temp -> next;
+            temp1 = temp1 -> next;
+            temp2 = temp2 -> next;
+        }
+
+        while(temp1 != NULL){
+            int sum = temp1 -> val + carry;
+            ListNode *newNode = new ListNode(sum % 10);
+            carry = sum / 10;
+            temp -> next = newNode;
+            temp = temp -> next;
+            temp1 = temp1 -> next;
+        }
+        while(temp2 != NULL){
+            int sum = temp2 -> val + carry;
+            ListNode *newNode = new ListNode(sum % 10);
+            carry = sum / 10;
+            temp -> next = newNode;
+            temp = temp -> next;
+            temp2 = temp2 -> next;
+        }
+        if(carry != 0){
+            ListNode *newNode = new ListNode(carry);
+            temp -> next = newNode;
+        }
+        return head -> next;
+    }
+};
