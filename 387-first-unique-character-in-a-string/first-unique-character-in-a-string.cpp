1af0f9b3bd7c7cd89b2b249af5df2c1a474e00e5
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-02-05 14:32:13 +0530

    Time: 59 ms (14.29%) | Memory: 13.1 MB (17.78%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        // O(N2)
+        // for(int i=0;i<s.size();i++){
+        //     bool is = false;
+        //     for(int j=0;j<s.size();j++){
+        //         if(i == j){
+        //             continue;
+        //         }
+        //         if(s[i] == s[j]){
+        //             is = true;
+        //             break;
+        //         }
+        //     }
+        //     if(is == false){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        map<char, int> mp;
         for(int i=0;i<s.size();i++){
-            bool is = false;
-            for(int j=0;j<s.size();j++){
-                if(i == j){
-                    continue;
-                }
-                if(s[i] == s[j]){
-                    is = true;
-                    break;
-                }
+            mp[s[i]]++;
+        }
+
+        for(int i=0;i<mp.size();i++){
+            cout<<mp[s[i]]<<endl;
+        }
+
+        string ans;
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]] == 1){
+                cout<<"Hiii"<<endl;
+                ans = s[i];
+                break;
             }
-            if(is == false){
-                return i;
+        }
+        cout<<"Hrllo"<<ans;
+        if(!ans.empty()){
+            for(int i=0;i<s.size();i++){
+                if(ans[0] == s[i]){
+                    return i;
+                }
             }
         }
         return -1;
