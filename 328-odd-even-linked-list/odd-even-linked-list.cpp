cc4f57a7ccbeafba400bec8232cf8e806a93376f
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-02-06 10:28:09 +0530

    Time: 11 ms (30.15%) | Memory: 14.1 MB (13.69%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,33 +11,52 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        // Brute Force
+        // Brute Force  --> TC O(2N) --> SC O(N) - extra space 
+        // if(head == NULL or head -> next == NULL){
+        //     return head;
+        // }
+        // vector<int> ans;
+        // ListNode *temp = head;
+        // while(temp != NULL and temp -> next != NULL){
+        //     ans.push_back(temp -> val);
+        //     temp = temp -> next -> next;
+        // }
+        // if(temp){
+        //     ans.push_back(temp -> val);
+        // }
+        // temp = head -> next;;
+        // while(temp != NULL and temp -> next != NULL){
+        //     ans.push_back(temp -> val);
+        //     temp = temp -> next -> next;
+        // }
+        // if(temp){
+        //     ans.push_back(temp -> val);
+        // }
+        // temp = head;
+        // int i = 0;
+        // while(temp != NULL){
+        //     temp -> val = ans[i++];
+        //     temp = temp -> next;
+        // }
+        // return head;
+
+
+
+
+
         if(head == NULL or head -> next == NULL){
             return head;
         }
-        vector<int> ans;
-        ListNode *temp = head;
-        while(temp != NULL and temp -> next != NULL){
-            ans.push_back(temp -> val);
-            temp = temp -> next -> next;
+        ListNode *odd = head;
+        ListNode *even = head -> next;
+        ListNode *evenNode = head -> next;
+        while(even != NULL and even -> next != NULL){
+            odd -> next = odd -> next -> next;
+            even -> next = even -> next -> next;
+            odd = odd -> next;
+            even = even -> next;
         }
-        if(temp){
-            ans.push_back(temp -> val);
-        }
-        temp = head -> next;;
-        while(temp != NULL and temp -> next != NULL){
-            ans.push_back(temp -> val);
-            temp = temp -> next -> next;
-        }
-        if(temp){
-            ans.push_back(temp -> val);
-        }
-        temp = head;
-        int i = 0;
-        while(temp != NULL){
-            temp -> val = ans[i++];
-            temp = temp -> next;
-        }
+        odd -> next = evenNode;
         return head;
     }
 };
