2d77c5e2fc6860a1d070e52feed2ff8adcd78bad
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-02-11 17:03:50 +0530

    Time: 65 ms (15.83%) | Memory: 23.6 MB (5.28%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -6,25 +6,46 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ #include<bits/stdc++.h>
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // TC -> O(n)
+        // ListNode *first = headA;
+        // ListNode *second = headB;
+        // while(first != second){
+        //     if(first == NULL){
+        //         first = headB;
+        //     }
+        //     else{
+        //         first = first -> next;
+        //     }
+        //     if(second == NULL){
+        //         second = headA;
+        //     }
+        //     else{
+        //         second = second -> next;
+        //     }
+        // }
+        // return first;
+
+
         ListNode *first = headA;
         ListNode *second = headB;
-        while(first != second){
-            if(first == NULL){
-                first = headB;
-            }
-            else{
-                first = first -> next;
-            }
-            if(second == NULL){
-                second = headA;
-            }
-            else{
-                second = second -> next;
+        map<ListNode*, int> mp;
+        while(first != NULL){
+            mp[first]++;
+            first = first -> next;
+        }
+        while(second != NULL){
+            mp[second]++;
+            second = second -> next;
+        }
+        for(auto i:mp){
+            if(i.second > 1){
+                return i.first;
             }
         }
-        return first;
+        return NULL;
     }
 };
