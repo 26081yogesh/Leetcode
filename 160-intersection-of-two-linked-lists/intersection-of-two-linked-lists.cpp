04aff0399c75ef20f07f4d4d33d9612af8f76d20
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-02-11 17:17:29 +0530

    Time: 41 ms (48.28%) | Memory: 17.1 MB (26.63%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -29,22 +29,66 @@ public:
         // }
         // return first;
 
+        // Brute Force Approach
+        // ListNode *first = headA;
+        // ListNode *second = headB;
+        // map<ListNode*, int> mp;
+        // while(first != NULL){
+        //     mp[first]++;
+        //     first = first -> next;
+        // }
+        // while(second != NULL){
+        //     mp[second]++;
+        //     second = second -> next;
+        // }
+        // for(auto i:mp){
+        //     if(i.second > 1){
+        //         return i.first;
+        //     }
+        // }
+        // return NULL;
 
-        ListNode *first = headA;
-        ListNode *second = headB;
-        map<ListNode*, int> mp;
-        while(first != NULL){
-            mp[first]++;
-            first = first -> next;
+        // Better Approach
+        int len1 = 1;
+        int len2 = 1;
+        ListNode *temp = headA;
+        while(temp -> next != NULL){
+            len1++;
+            temp = temp -> next;
         }
-        while(second != NULL){
-            mp[second]++;
-            second = second -> next;
+        temp = headB;
+        while(temp -> next != NULL){
+            len2++;
+            temp = temp -> next;
         }
-        for(auto i:mp){
-            if(i.second > 1){
-                return i.first;
+        ListNode *temp1 = headA;
+        ListNode *temp2 = headB;
+        bool isT1 = false;
+        if(len1 < len2){
+            isT1 = true;
+        }
+
+        int diff = abs(len2 - len1);
+        int i = 0;
+        if(isT1){    
+            while(i < diff){
+                temp2 = temp2 -> next;
+                i++;
             }
+        }
+        else{
+            while(i < diff){
+                temp1 = temp1 -> next;
+                i++;
+            }
+        }
+
+        while(temp1 != NULL and temp2 != NULL){
+            if(temp1 == temp2){
+                return temp1;
+            }
+            temp1 = temp1 -> next;
+            temp2 = temp2 -> next;
         }
         return NULL;
     }
