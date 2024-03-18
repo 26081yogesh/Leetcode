e4df4883fe03c7ee61ef1212299c1a6079e57f87
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-03-18 13:57:52 +0530

    Time: 4 ms (67.79%) | Memory: 13.5 MB (13.11%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        map<int, int> ump;
-        for(int i=0;i<nums.size();i++){
-            if(ump[nums[i]] != 2){
-                ump[nums[i]]++;
+        // Brute Force = extra space O(n)
+        // map<int, int> ump;
+        // for(int i=0;i<nums.size();i++){
+        //     if(ump[nums[i]] != 2){
+        //         ump[nums[i]]++;
+        //     }
+        // }
+        // vector<int> v;
+        // for(int i=0;i<nums.size();i++){
+        //     if(ump.find(nums[i]) != ump.end() and ump[nums[i]] > 0){
+        //         ump[nums[i]]--;
+        //         v.push_back(nums[i]);
+        //     }
+        // }
+        // nums.clear();
+        // for(auto i:v){
+        //     nums.push_back(i);
+        // }
+        // return v.size();
+
+
+        int i=0;
+        for(auto n:nums){
+            if(i < 2 || n > nums[i-2]){
+                nums[i++] = n;
             }
         }
-        vector<int> v;
-        for(int i=0;i<nums.size();i++){
-            if(ump.find(nums[i]) != ump.end() and ump[nums[i]] > 0){
-                ump[nums[i]]--;
-                v.push_back(nums[i]);
-            }
-        }
-        nums.clear();
-        for(auto i:v){
-            nums.push_back(i);
-        }
-        return v.size();
+        return i;
     }
 };
