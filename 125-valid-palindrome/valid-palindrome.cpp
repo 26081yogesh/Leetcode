126b2eb17640e2bfc4bed01c9989a1ab7915cbc2
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-03-18 14:27:25 +0530

    Time: 4 ms (80.04%) | Memory: 8.7 MB (61.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,27 +2,45 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        string temp;
-        cout<<s<<endl<<endl;
-        cout<<temp<<endl<<endl;
-        for(int i=0;i<s.size();i++){
-            if(s[i] >= 'a' and s[i] <= 'z' or s[i] >= '0' and s[i] <= '9'){
-                temp += s[i];
-            }
-        }
-        s = temp;
+        // transform(s.begin(), s.end(), s.begin(), ::tolower);
+        // string temp;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i] >= 'a' and s[i] <= 'z' or s[i] >= '0' and s[i] <= '9'){
+        //         temp += s[i];
+        //     }
+        // }
+        // s = temp;
+
+        // int start = 0;
+        // int end = s.size()-1;
+
+        // while(start<end){
+        //     swap(s[start++], s[end--]);
+        // }
+        // cout<<s<<endl<<temp;
+        // if(s == temp){
+        //     return true;
+        // }
+        // return false;
+
 
         int start = 0;
         int end = s.size()-1;
-
-        while(start<end){
-            swap(s[start++], s[end--]);
+        while(start < end){
+            if(!isalnum(s[start])){
+                start++;
+            }
+            else if(!isalnum(s[end])){
+                end--;
+            }
+            else if(tolower(s[start]) != tolower(s[end])){
+                return false;
+            }
+            else{
+                start++;
+                end--;
+            }
         }
-        cout<<s<<endl<<temp;
-        if(s == temp){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
