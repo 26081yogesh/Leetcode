34c2015611d9e4747a491a69cdcb5a53dfa15af7
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-03-19 11:01:32 +0530

    Time: 11 ms (37.14%) | Memory: 17.8 MB (89.97%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        unordered_map<int, int> ump;
-        for(int i=0;i<numbers.size();i++){
-            int diff = target-numbers[i];
-            cout<<diff<<endl;
-            if(ump.find(diff) == ump.end()){
-                ump[numbers[i]] = i;
+        // Brute force using extra space O(n)
+        // unordered_map<int, int> ump;
+        // for(int i=0;i<numbers.size();i++){
+        //     int diff = target-numbers[i];
+        //     cout<<diff<<endl;
+        //     if(ump.find(diff) == ump.end()){
+        //         ump[numbers[i]] = i;
+        //     }
+        //     else{
+        //         return {ump[diff]+1, i+1};
+        //     }
+        // }
+        // return {-1, -1};
+
+        int start = 0;
+        int end = numbers.size()-1;
+        while(start < end){
+            int sum = numbers[start]+numbers[end];
+            if(sum == target){
+                return {start+1, end+1};
+            }
+            else if(sum > target){
+                end--;
             }
             else{
-                return {ump[diff]+1, i+1};
+                start++;
             }
         }
-        return {-1, -1};
+        return {};
     }
 };
