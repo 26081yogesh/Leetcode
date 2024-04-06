4f3858bd40ac649b82421442fae10678424ce18f
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2024-04-06 15:59:00 +0530

    Time: 4 ms (67.02%) | Memory: 13.4 MB (26.83%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -12,30 +12,52 @@ class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         // Brute Force Approach
-        vector<int> v1;
-        vector<int> v2;
-        ListNode *temp = head;
-        while(temp != NULL){
-            if(temp -> val < x){
-                v1.push_back(temp -> val);
+        // vector<int> v1;
+        // vector<int> v2;
+        // ListNode *temp = head;
+        // while(temp != NULL){
+        //     if(temp -> val < x){
+        //         v1.push_back(temp -> val);
+        //     }
+        //     else{
+        //         v2.push_back(temp -> val);
+        //     }
+        //     temp = temp -> next;
+        // }
+
+        // ListNode *res = new ListNode(-100);
+        // head = res;
+        // temp = head;
+        // for(auto i:v2){
+        //     v1.push_back(i);
+        // }
+        // for(auto i:v1){
+        //     ListNode *newNode = new ListNode(i);
+        //     temp -> next = newNode;
+        //     temp = temp -> next;
+        // }
+        // return head->next;
+
+
+        // Better approach
+        ListNode *l1 = head;
+        ListNode *s1 = new ListNode(-100);
+        ListNode *s2 = new ListNode(-100);
+        ListNode *temp1 = s1;
+        ListNode *temp2 = s2;
+        while(l1 != NULL){
+            if(l1 -> val < x){
+                temp1 -> next = l1;
+                temp1 = temp1 -> next;
             }
             else{
-                v2.push_back(temp -> val);
+                temp2 -> next = l1;
+                temp2 = temp2 -> next;
             }
-            temp = temp -> next;
+            l1 = l1 -> next;
         }
-
-        ListNode *res = new ListNode(-100);
-        head = res;
-        temp = head;
-        for(auto i:v2){
-            v1.push_back(i);
-        }
-        for(auto i:v1){
-            ListNode *newNode = new ListNode(i);
-            temp -> next = newNode;
-            temp = temp -> next;
-        }
-        return head->next;
+        temp2 -> next = NULL;
+        temp1 -> next = s2 -> next;
+        return s1 -> next;
     }
 };
